d16d24fb52375e72365ac0ed4549155ac419e766
Alex Krolick <alexander.krolick@gmail.com>
2018-06-25 22:33:05 -0700

    Pretty init script

--- bin.js
+++ bin.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
-require("./init-package-json")(
-  process.cwd(),
-  "",
-  {},
-  (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(JSON.stringify(data, null, 2));
-    console.log("\n\nYour React build system is ready to go!\n\nNow run:\n")
-    console.log("  Install dependencies:")
-    console.log("    npm install\n")
-    console.log("  Production build:")
-    console.log("    npm run build\n")
-    console.log("  Development build (watcher):")
-    console.log("    npm run dev\n")
+const init = require("./init-package-json");
+
+const yes =
+  process.execArgv.includes("--yes") || process.execArgv.includes("-y");
+
+init(process.cwd(), "", { yes }, function(err, data) {
+  if (err) {
+    console.error(err);
+    return;
   }
-);
+  console.log(JSON.stringify(data, null, 2));
+  console.log("\n\nYour React build system is ready to go!\n\nNow run:\n");
+  console.log("  ⚡️ Install dependencies:");
+  console.log("     npm install\n");
+  console.log("  🚦 Production build:");
+  console.log("     npm run build\n");
+  console.log("  🚧 Development build (watcher):");
+  console.log("     npm run dev\n");
+});
+
